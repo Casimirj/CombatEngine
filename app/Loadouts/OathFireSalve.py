2a0c492692603e352277bd6430f7873d1c348dd5
jayrhaft <31804723+jayrhaft@users.noreply.github.com>
2025-07-15 20:04:17 -0400

    Update OathFireSalve.py
    changed to infernal cape for some decent comparision to BIS

--- app/Loadouts/OathFireSalve.py
+++ app/Loadouts/OathFireSalve.py
@@ -10,13 +10,13 @@ stats = {
     'ranged_level': 99,
     'prayer_level': 99,
 
-    'slash_attack_bonus': 57,
-    'stab_attack_bonus': 19,
-    'crush_attack_bonus': 19,
+    'slash_attack_bonus': 60,
+    'stab_attack_bonus': 22,
+    'crush_attack_bonus': 22,
     'magic_attack_bonus': 0,
     'ranged_attack_bonus': 0,
 
-    'melee_strength_bonus': 47,
+    'melee_strength_bonus': 51,
     'magic_strength_bonus': 0,
     'ranged_strength_bonus': 0,
 }
